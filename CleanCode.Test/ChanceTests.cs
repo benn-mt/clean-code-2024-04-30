4470cc201ad5f955267815009061b1ece920af86
Ben Noble <ben.noble@madetech.com>
2024-04-30 15:43:19 +0100

    Comparison with object

--- CleanCode.Test/ChanceTests.cs
+++ CleanCode.Test/ChanceTests.cs
@@ -6,5 +6,7 @@ public class ChanceTests
     public void CanBeComparedForEquality()
     {
         Assert.That(new Chance(0.5).Equals(new Chance(0.5)));
+        Assert.That(new Chance(1.0).Equals(new Chance(0.0)), Is.False);
+        Assert.That(new Chance(1.0).Equals(new object()), Is.False);
     }
 }
--- CleanCode/Chance.cs
+++ CleanCode/Chance.cs
@@ -2,12 +2,22 @@ namespace CleanCode;
 
 public class Chance
 {
+    private readonly double likelihood;
+
     public Chance(double likelihood)
     {
+        this.likelihood = likelihood;
     }
 
-    public override bool Equals(object obj)
+    public override bool Equals(object? obj)
     {
-        return true;
+        if (obj is not Chance){
+            return false;
+        }
+        return this.Equals(obj as Chance);
+    }
+
+    public bool Equals(Chance other){
+        return this.likelihood == other.likelihood;
     }
 }
