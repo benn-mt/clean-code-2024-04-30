62fb065b877d88fcdb4a159cca001dab98ff615b
Ben Noble <ben.noble@madetech.com>
2024-05-02 08:58:27 +0100

    Added other units

--- CleanCode.Test/LengthTests.cs
+++ CleanCode.Test/LengthTests.cs
@@ -3,6 +3,9 @@ namespace CleanCode.Test;
 public class LengthTests{
     [Test]
     public void KnownUnitConverstions_ExtensionMethods(){
-        Assert.AreEqual(Lengths.INCH.S(12.0), Lengths.FOOT.S(1));
+        Assert.AreEqual(Lengths.INCH.S(12), Lengths.FOOT.S(1));
+        Assert.AreEqual(Lengths.FOOT.S(3), Lengths.YARD.S(1));
+        Assert.AreEqual(Lengths.YARD.S(220), Lengths.FURLONG.S(1));
+        Assert.AreEqual(Lengths.FURLONG.S(8), Lengths.MILE.S(1));
     }
 }
